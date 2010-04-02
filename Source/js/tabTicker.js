87e5266c4a3d029b02191202a1006f76a07d2116
Richard Harrison <me@rickyh.co.uk>
2010-04-02 15:58:52 +0100

    YAM

--- Source/js/tabTicker.js
+++ Source/js/tabTicker.js
@@ -1,11 +1,9 @@
 /*
-description: tabAlert
-	- When a user is away playing in another browser tab you
-	  can still feed them with important imformation. This is
-	  a great extension to AJAX calls.
+description:
+      - tabAlert: When a user is away playing in another browser tab you can still feed them with important imformation. This is a great extension to AJAX calls.
 
 authors:
-  - Ricky Harrison	http://www.rickyh.co.uk
+  - Ricky Harrison http://www.rickyh.co.uk
 
 license:
   - MIT-style license
@@ -15,38 +13,6 @@ requires:
 
 provides:
   - tabAlert
-
-examples:
-
-	1.	Updates the document.title to until the user returns to the tab.
-	
-				var ex1 = new tabAlert({
-					text: 'You Have one new message' 
-				});
-	
-	2. Goal Flash Ticker.
-				
-				var ex2 = yourInstance = new tabAlert({
-					text: 'GOAL FLASH: Manchester United 0:1 LEEDS UNITED (Beckford 19)',
-					ticker: true
-				});
-				
-				
-				// to update the text use
-				ex2.updateText("FINAL SCORE: Manchester United 0:1 LEEDS UNITED");
-	
-	
-	3. Goal Flash Ticker. Flashes live data but only loops once!
-	
-				var ex3 = yourInstance = new tabAlert({
-					text: 'GOAL FLASH: Manchester United 0:1 LEEDS UNITED (Beckford 19)',
-					ticker: true,
-					onLoop: function(){
-						this.revert();
-					}
-				});
-	 
-
 */
 
  
